70d745ff614d4187ec2147f7016610a7fcf59fb2
Blake Collins <blakeac369@gmail.com>
2023-06-17 17:06:35 -0700

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
       <Exercises 
         setExercises={setExercises}
         bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
+        setBodyPart={exercises}
       />
     </Box>
   )
